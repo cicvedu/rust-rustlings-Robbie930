34f9ea021b8bc0f98fa7baed08f7505d29e49c3f
Robbie930 <Hongyuan_02@outlook.com>
2023-08-08 18:04:11 +0800

    clippy2.rs

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,13 +3,13 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let mut res = 42;
-    let option = Some(12);
-    for x in option {
-        res += x;
+    let option: Option<Vec<i32>> = Some(vec![12]);
+    match option {
+        Some(vec) => vec.iter().for_each(|x| res += *x),
+        None => (),
     }
     println!("{}", res);
 }
