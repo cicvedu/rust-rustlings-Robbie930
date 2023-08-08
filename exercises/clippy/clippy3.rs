d3e787383a2b84a766ada425f4d878a22bfe8006
Robbie930 <Hongyuan_02@outlook.com>
2023-08-08 18:05:02 +0800

    clippy3.rs

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -10,22 +10,25 @@
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        my_option.unwrap();
+        println!("The value is: None"); // 修改输出为 "The value is: None"
+    } else {
+        let my_val = my_option.unwrap();
+        println!("The value is: {:?}", my_val); // 使用 {:?} 进行调试输出
     }
 
-    let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+    let my_arr = [
+        -1, -2, -3, -4, -5, -6,
     ];
-    println!("My array! Here it is: {:?}", my_arr);
+    println!("My array! Here it is: {:?}", my_arr); // 修改输出格式为 "[my_arr]"
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    let my_empty_vec: Vec<i32> = vec![];
+    println!("This Vec is empty, see:? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
+    let temp = value_a;
     value_a = value_b;
-    value_b = value_a;
-    println!("value a: {}; value b: {}", value_a, value_b);
+    value_b = temp;
+    println!("value a: {}, value b: {}", value_a, value_b); // 修改输出格式为 "{}"
 }
+
